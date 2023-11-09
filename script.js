83118418cee4a199786d7e3225277a594d1e158b
Ricardo <146256152+ricode-icaito@users.noreply.github.com>
2023-11-08 20:30:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/*alerta en una linea*/ 
+alert("Hello World");
+
+/*hello world en una linea*/ 
+document.write("Hello World ");
+
+/*suma de 3 + 5 en una linea*/
+document.write(3+5);
+
+/*solicitar un nombre y ver hola + nombreUsuario*/ 
+var nombre = prompt("Dime tu  nombre");
+alert("Hola " + nombre); 
+
+/*suma de dos numeros, mostrando el resultado */ 
+var numero1 = prompt("Ingrese el primer numero:");
+var numero2 = prompt("Ingrese el sugundo numero:");
+var suma = Number(numero1) + Number(numero2);alert("Resultado "+suma)
